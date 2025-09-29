e241af550c0be1bd37a442840d86196a6f37bb77
Mark Timofeev <fedorkrikman@gmail.com>
2025-09-29 10:18:22 +0300

    test

--- JupyterNotebooks/test.py
+++ JupyterNotebooks/test.py
@@ -1,6 +1,7 @@
 import sys, os, numpy as np, pandas as pd, matplotlib, seaborn as sns, PIL
 import tensorflow as tf, tensorflow_datasets as tfds
 
+###
 matplotlib.rcParams['figure.figsize'] = [9, 6]
 print("PY:", sys.executable)
 print("TF:", tf.__version__, "| TFDS:", tfds.__version__)
